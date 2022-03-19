26c732eea30fbf7c4806654fdeb083da74f0cb78
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-03-19 11:30:49 +0100

    order and payment

--- src/dtos/order.dto.ts
+++ src/dtos/order.dto.ts
@@ -1,18 +1,21 @@
 /* eslint-disable prettier/prettier */
 
-import { IsString, IsArray, IsObject, IsNumber } from 'class-validator';
+import { IsString, IsArray, IsObject, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateOrderDTO {
   @IsString()
   customerId: string;
 
   @IsString()
+  @IsOptional()
   paymentId: string;
 
   @IsString()
+  @IsOptional()
   paymentStatus: string;
 
   @IsString()
+  @IsOptional()
   deliveryStatus: string;
 
   @IsString()
